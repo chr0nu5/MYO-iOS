caa7950c43501641e84f4fa86ea5606640a58813
Tarang Khanna <tarangalbert@gmail.com>
2015-02-02 14:44:43 -0500

    Myo Initial iOS
    Intiital commit- starting with MYO + iOS dev

---
+++ MYO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MYO
+//
+//  Created by Tarang Khanna on 2/2/15.
+//  Copyright (c) 2015 Thacked. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
